82d319e2e680aa9fba074c0a1c84d059ee4c82e1
defless <simon.deflesschouwer@mmibordeaux.com>
2020-12-25 22:44:52 +0100

    feat(front): update edit form

--- packages/front/src/components/Edit.vue
+++ packages/front/src/components/Edit.vue
@@ -1,22 +1,31 @@
 <template>
-  <div id="editMovie">
-    <label>Title:
-      <input v-model="movie.title"/>
-    </label>
-    <label>Date:
-      <input v-model="movie.year"/>
-    </label>
-    <label>Synopsis:
-     <textarea v-model="movie.description"></textarea>
-    </label>
-    <label>Score:
-      <input v-model="movie.score"/>
-    </label>
-    <label>Poster:
-      <input v-model="movie.poster"/>
-    </label>
-    <button v-on:click="editM">Edit</button>
-  </div>
+  <form class="col-md-4">
+    <h2>Edit {{movie.title}}</h2>
+    <div class="row mb-3 mt-5">
+      <label class="col-md-3">Title</label>
+      <input class="form-control col-md-9" v-model="movie.title" />
+    </div>
+    <div class="row mb-3">
+      <label class="col-md-3">Release date</label>
+      <input class="form-control col-md-9" v-model="movie.year" />
+    </div>
+    <!-- <label>Director
+      <input v-model="movie.director" />
+    </label> -->
+    <div class="row mb-3">
+      <label class="col-md-3">Synopsis</label>
+      <textarea class="form-control col-md-9" v-model="movie.description"></textarea>
+    </div>
+    <div class="row mb-3">
+      <label class="col-md-3">Score: {{movie.score}}</label>
+      <input type="range" class="form-range col-md-9 slider" min="0" max="5" step="1" v-model="movie.score">
+    </div>
+    <div class="row mb-3">
+      <label class="col-md-3">Picture</label>
+      <input class="form-control col-md-9" v-model="movie.poster" />
+    </div>
+    <button class="btn btn-primary" v-on:click="editM">Edit</button>
+  </form>
 </template>
 
 <script>
